8eae506fc6dbe8cfa7a4df267a0d1644e6d86068
Svetlana Linuxenko <linuxenko@yahoo.com>
2016-10-27 23:10:13 +0300

    [init] base

---
+++ server/index.js
@@ -0,0 +1,15 @@
+
+var PORT = process.env.PORT || 8080;
+
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req,res) {
+  res.end('hello world');
+});
+
+
+app.listen(PORT, function() {
+  console.log('listening on port ' + PORT);
+});
